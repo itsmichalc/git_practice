4aeeaa7b8d461be1c30af22ab9be1238adfe5584
Michal Cohen <micha.cohen@openweb.com>
2022-12-19 12:21:44 +0200

    new input bar

--- typescript-trial/src/App.tsx
+++ typescript-trial/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputField from "./components/InputField";
 
 function App() {
   return (
     <div className="App">
       <span className = "heading">Taskify</span>
+      <InputField/>
     </div>
   );
 }
